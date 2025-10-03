2e96a2c6ca5fcaeaf8c84bcaaa49ab776707828a
OksanaBannova <97969086+OksanaBannova@users.noreply.github.com>
2025-10-04 00:37:20 +0700

    Update ControllableStates.jsx

--- src/components/Molecules/MUI/ControllableStates.jsx
+++ src/components/Molecules/MUI/ControllableStates.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 /*import { Autocomplete, TextField } from "@mui/material";*/
 import { useDispatch } from "react-redux";
 import { setForm } from "../../../features/formTicketsSlice";
-import { makeStyles } from "@mui/styles";
+/*import { makeStyles } from "@mui/styles";*/
 
 const ControllableStates = ({
   type,
