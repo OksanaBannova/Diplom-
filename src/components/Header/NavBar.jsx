a8c5921f6bbbab3075bd3edfadf39ab00778a5ce
OksanaBannova <97969086+OksanaBannova@users.noreply.github.com>
2025-10-04 01:45:05 +0700

    Update NavBar.jsx

--- src/components/Header/NavBar.jsx
+++ src/components/Header/NavBar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-/*import { HashLink as Link } from "react-router-hash-link";*/
+import { HashLink as Link } from "react-router-hash-link";
 
 const NavBar = () => {
   return (
