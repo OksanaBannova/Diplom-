4d15760cf6532aa31c6c5883ba6b2d3055d343d9
Oksana Bannova <oksanchik2170@yandex.ru>
2025-10-05 10:00:17 +0300

    app

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+/*function App() {
   const [count, setCount] = useState(0)
 
   return (
@@ -32,4 +32,25 @@ function App() {
   )
 }
 
-export default App
+export default App*/
+ 
+
+const Main = (props) => {
+  const location = useLocation();
+
+  const classBased =
+    location.pathname === "/Diploma-FFE-Train-Tickets"
+      ? "main-content__home-page"
+      : "main-content__wrap d-flex";
+  return (
+    <main className="main container-fluid">
+      <div className="row">
+        <div className="col col-lg p-0">
+          <div className={classBased}>{props.children}</div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Main;
