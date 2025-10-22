5c5999e96710ea90702f5b4fd0a85f18cd99d6a1
Oksana Bannova <oksanchik2170@yandex.ru>
2025-10-22 08:52:25 +0300

    optim

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,49 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer'
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      filename: 'dist/stats.html',
+      open: false,
+      gzipSize: true
+    })
+  ],
+  
+  build: {
+    minify: 'terser',
+    target: 'es2015',
+    
+    // Оптимизация для ваших компонентов
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          ui: ['react-router-dom'], // Учитывая что у вас есть роутинг
+          charts: ['recharts'] // Если используете графики
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js'
+      }
+    },
+    
+    chunkSizeWarningLimit: 800
+  },
+  
+  optimizeDeps: {
+    include: ['react', 'react-dom', 'react-router-dom']
+  },
+  
+  server: {
+    port: 3000,
+    open: true
+  },
+
+
+  // ... остальная конфигурация
+  base: '/Diplom/', // Имя вашего репозитория
 })
