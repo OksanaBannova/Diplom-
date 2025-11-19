df7050da69bb9878b5500ce6d232fb1eb0e60eb0
OksanaBannova <97969086+OksanaBannova@users.noreply.github.com>
2025-11-19 17:47:12 +0300

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 
-import { api } from "./features/myApi";
-import { other } from "./features/otherApi";
-import formReducer from "./features/formTicketsSlice";
-import catalogTrainsReducer from "./features/catalogTrainsSlice";
-import passengersReducer from "./features/passengersSlice";
+import { api } from "../features/myApi";
+import { other } from "../features/otherApi";
+import formReducer from "../features/formTicketsSlice";
+import catalogTrainsReducer from "../features/catalogTrainsSlice";
+import passengersReducer from "../features/passengersSlice";
+
+const rtkMiddleware = (getDefaultMiddleware) =>
+  getDefaultMiddleware().concat(api.middleware).concat(other.middleware);
 
 const store = configureStore({
   reducer: {
@@ -15,10 +18,8 @@ const store = configureStore({
     catalogTrains: catalogTrainsReducer,
     passengers: passengersReducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(api.middleware, other.middleware),
+  middleware: rtkMiddleware,
 });
+export default store;
 
 setupListeners(store.dispatch);
-
-export default store;
