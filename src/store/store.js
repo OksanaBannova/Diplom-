23431b8ca5fb37e72d201bde2b5993b02d851767
OksanaBannova <97969086+OksanaBannova@users.noreply.github.com>
2025-11-19 08:18:02 +0300

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,14 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 
-import { api } from "./features/myApi"; // Исправлены пути
+import { api } from "./features/myApi";
 import { other } from "./features/otherApi";
 import formReducer from "./features/formTicketsSlice";
 import catalogTrainsReducer from "./features/catalogTrainsSlice";
 import passengersReducer from "./features/passengersSlice";
-
-const rtkMiddleware = (getDefaultMiddleware) =>
-  getDefaultMiddleware().concat(api.middleware).concat(other.middleware);
 
 const store = configureStore({
   reducer: {
