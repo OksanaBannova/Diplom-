b248a100b8bb26926c76c64b38f5d5c0c3a9afe0
OksanaBannova <97969086+OksanaBannova@users.noreply.github.com>
2025-11-19 08:16:55 +0300

    Update otherApi.js

--- src/features/otherApi.js
+++ src/features/otherApi.js
@@ -4,63 +4,27 @@ export const other = createApi({
   reducerPath: "other",
   baseQuery: fetchBaseQuery({
     baseUrl: "https://students.netoservices.ru/fe-diplom/",
-    tagTypes: ["Order", "Subscriber"],
   }),
+  tagTypes: ["Order", "Subscriber"],
   endpoints: (builder) => ({
-    /* addSubscriber: builder.mutation({
-      queryFn: async (data) => {
-        console.log(data, "data");
-        const { email, ...body } = data;
-        try {
-          let response = await fetch(`subscribe?email=${email}`, {
-            method: "post",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(body),
-          });
-          console.log(response, "response");
-        } catch {
-          console.log("error");
-        }
-      },
-    }),*/
+    addSubscriber: builder.mutation({
+      query: (data) => ({
+        url: "subscribe",
+        method: "POST",
+        body: data,
+      }),
+      invalidatesTags: ["Subscriber"],
+    }),
+    
     addOrder: builder.mutation({
-      query: ({ body }) => ({
-        //это работает
-        url: `/order`,
+      query: (body) => ({
+        url: "order",
         method: "POST",
         body,
       }),
       invalidatesTags: ["Order"],
-      /*queryFn: async (data) => {// а это работает некорректно
-        console.log(data, "data");
-        const { ...body } = data;
-        try {
-          let response = await fetch(`/order`, {
-            method: "post",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(body), // Добавление заказа
-          });
-          console.log(response, "response");
-        } catch {
-          console.log("error");
-        }
-      },
-      invalidatesTags: ["Order"],*/
     }),
   }),
 });
 
 export const { useAddSubscriberMutation, useAddOrderMutation } = other;
-/**    */
-
-/**      */
-
-/**     /* async onQueryStarted({body}, {dispatch, queryFulfilled}) {
-        const patchResult = dispatch(
-            other.util.updateQueryData('getOrder',  (draft) => Object.assign(draft, body)))
-        try {
-          await queryFulfilled
-        } catch {
-          patchResult.undo()
-        }
-      },*/
